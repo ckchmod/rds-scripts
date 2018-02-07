df3b236a99d674f401774c1f611c0f478f5b7969
ckchmod <charlestreykang@gmail.com>
2018-02-07 11:20:02 -0800

    p53 network

--- ge_and_qian.m
+++ ge_and_qian.m
@@ -3,9 +3,9 @@ load('p53_network/F.mat')
 load('p53_network/nv.mat')
 load('p53_network/varF.mat')
 nsteps = 1;
-x = [1, 1, 1, 1, 1];
+x = [1, 1, 0, 0, 1];
 next = bnRun(x,F,varF,nv,nsteps);
-bin2dec(num2str(next(end,:)))+1
+bin2dec(num2str(next(end,:))) 
 
-[A,Avec] = bnAsparse(F,varF,nv)
-spy(A)
+[A,Avec] = bnAsparse(F,varF,nv);
+spy(A);
