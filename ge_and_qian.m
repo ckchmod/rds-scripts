d490e3183472aeb2f380c30fe5db65b7bd22ac11
ckchmod <charlestreykang@gmail.com>
2018-02-07 16:38:19 -0800

    blunder

--- ge_and_qian.m
+++ ge_and_qian.m
@@ -3,7 +3,7 @@ load('p53_network/F.mat')
 load('p53_network/nv.mat')
 load('p53_network/varF.mat')
 nsteps = 1;
-x = [1, 1, 0, 0, 1];
+x = [0, 0, 1, 1, 0];
 next = bnRun(x,F,varF,nv,nsteps);
 bin2dec(num2str(next(end,:))) 
 
